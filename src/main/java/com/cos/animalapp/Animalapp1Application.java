339d92ac6dd4ff33d3dfa8d980ed6c64c50f79c6
choi <choi.dahye424@gmail.com>
2021-11-08 12:41:36 +0900

    코드 수정

---
+++ src/main/java/com/cos/animalapp/Animalapp1Application.java
@@ -0,0 +1,13 @@
+package com.cos.animalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Animalapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Animalapp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cos/animalapp/Animalapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.animalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Animalapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
